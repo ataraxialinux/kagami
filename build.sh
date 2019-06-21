2a95cab84bdd637649997583ccccc0ecae512748
Ishimoto Shinobu <nagakamira@gmail.com>
2019-06-22 01:23:41 +0300

    some
    Signed-off-by: Ishimoto Shinobu <nagakamira@gmail.com>

--- build.sh
+++ build.sh
@@ -4,20 +4,71 @@ VERSION=20190622
 
 set -e
 
-echo "Compiling kagami package manager version $VERSION"
+die() {
+	echo -e "ERROR: $@"
+	exit 1
+}
 
-rm -rf kagami vercmp
+build_src() {
+	echo "Compiling kagami package manager version $VERSION"
 
-echo "Compiling scripts"
-for scripts in kagami; do
-	cp $scripts.in $scripts
-	sed -i $scripts -e "s|@VERSION[@]|$VERSION|g"
+	rm -rf kagami vercmp
+
+	echo "Compiling scripts"
+	for scripts in kagami; do
+		cp $scripts.in $scripts
+		sed -i $scripts -e "s|@VERSION[@]|$VERSION|g"
+	done
+
+	echo "Compiling helpers"
+	${CROSS_COMPILE}cc $CFLAGS -static helpers/strlcpy.c helpers/vercmp.c helpers/dewey.c -o vercmp >&2
+
+	echo "Done!"
+}
+
+install_src() {
+	install -Dvm775 kagami "${DESTDIR}"${PREFIX}/bin/kagami
+	install -Dvm775 vercmp "${DESTDIR}"${PREFIX}/bin/vercmp
+}
+
+DESTDIR=
+PREFIX=/usr/local
+
+while getopts :IBPD: options; do
+	case $options in
+		B)
+			mode=build
+			;;
+		I)
+			mode=install
+			;;
+		P)
+			PREFIX="${OPTARG}"
+			;;
+		D)
+			DESTDIR="${OPTARG}"
+			;;
+		:)
+			die "Option '-${OPTARG}' needs an argument"
+			;;
+		\?)
+			die "Option '-${OPTARG}' is illegal"
+			;;
+	esac
 done
+if [ "$#" -eq 0 ]; then
+	die "Specify options. To list available options use: kagami -h"
+fi
+shift $((OPTIND - 1))
 
-echo "Compiling helpers"
-${CROSS_COMPILE}cc $CFLAGS -static helpers/strlcpy.c helpers/vercmp.c helpers/dewey.c -o vercmp >&2
-
-echo "Done!"
+case $mode in
+	build)
+		build_src
+		;;
+	install)
+		install_src
+		;;
+esac
 
 exit 0
 
