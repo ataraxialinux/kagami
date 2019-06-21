8d400c7fb2342436ab81a11ac9a62ee385c78ff1
Ishimoto Shinobu <nagakamira@gmail.com>
2019-06-22 00:56:27 +0300

    add new kagami
    Signed-off-by: Ishimoto Shinobu <nagakamira@gmail.com>

--- build.sh
+++ build.sh
@@ -4,15 +4,6 @@ set -e
 
 # print logo
 
-echo "Cleaning up..."
-rm -rf vercmp hakai minaoshi tsukuri
-
-echo "Generating scripts"
-for scripts in hakai minaoshi tsukuri; do
-	cp $scripts.in $scripts
-	sed -i $scripts -e "s|@VERSION[@]|$VERSION|g"
-done
-
 echo "Compiling helpers"
 ${CROSS_COMPILE}cc $CFLAGS -static helpers/strlcpy.c helpers/vercmp.c helpers/dewey.c -o vercmp >&2
 
