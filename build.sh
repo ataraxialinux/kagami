91308cb9c6188bac4432b26b16e7f1ecf6531b14
Ishimoto Shinobu <nagakamira@gmail.com>
2019-06-22 01:16:53 +0300

    some tweaks
    Signed-off-by: Ishimoto Shinobu <nagakamira@gmail.com>

--- build.sh
+++ build.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
 
+VERSION=20190622
+
 set -e
 
-# print logo
+echo "Compiling kagami package manager version $VERSION"
+
+rm -rf kagami vercmp
+
+echo "Compiling scripts"
+for scripts in kagami; do
+	cp $scripts.in $scripts
+	sed -i $scripts -e "s|@VERSION[@]|$VERSION|g"
+done
 
 echo "Compiling helpers"
 ${CROSS_COMPILE}cc $CFLAGS -static helpers/strlcpy.c helpers/vercmp.c helpers/dewey.c -o vercmp >&2
